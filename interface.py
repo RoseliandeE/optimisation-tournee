06f3d0f98a5efcdcadec19c4da876d75dff10590
RoseliandeE <roseliande.cusson@eiffage.com>
2026-03-16 16:33:57 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -208,7 +208,8 @@ if check_mot_de_passe():
         
             except FileNotFoundError as e:
                 st.error(f"Fichier manquant : {e}. Veuillez vérifier que tous les CSV sont présents.")
-                return pd.DataFrame(),pd.DataFrame()
+                st.session_state.site =pd.DataFrame()
+                st.session_state.duration  = pd.DataFrame()
             
             
         
